99c1ffc1cd7b240ae766f003dfa76ba7d1ca1606
LEE CHAE EUN <chaeeun8837@kw.ac.kr>
2025-05-16 11:13:38 +0900

    아이콘드래그

--- app/src/main/java/com/example/team7_realhelper/OverlayService.java
+++ app/src/main/java/com/example/team7_realhelper/OverlayService.java
@@ -7,6 +7,8 @@ import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,6 +19,7 @@ import androidx.annotation.Nullable;
 public class OverlayService extends Service {
     private WindowManager windowManager;  // 윈도우 관리하는 시스템 서비스 객체
     private ImageView overlayIcon;   // 오버레이 띄울 아이콘
+    private WindowManager.LayoutParams params;
 
     @Override
     public void onCreate() {
@@ -28,7 +31,7 @@ public class OverlayService extends Service {
         overlayIcon.setImageResource(R.drawable.start_icon);  // 이미지 설정
 
         // 띄울 뷰의 크기, 타입 등등 정의
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        params = new WindowManager.LayoutParams(
                 150, 150,  // 크기
                 Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ?
                         WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY :
@@ -41,8 +44,41 @@ public class OverlayService extends Service {
         params.x = 100;
         params.y = 100;
 
-        windowManager.addView(overlayIcon, params);  // 화면에 띄움
+        // 드래그 리스너
+        overlayIcon.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
 
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:   // 드래그 시작시 
+
+                        // 시작 위치 저장
+                        initialX=params.x;
+                        initialY=params.y;
+                        initialTouchX=event.getRawX();
+                        initialTouchY=event.getRawY();
+                        return true;
+
+
+                    case MotionEvent.ACTION_MOVE:   // 이동하면
+                        // 움직인 거리 계산
+                        params.x=initialX+(int)(event.getRawX()-initialTouchX);
+                        params.y=initialY+(int)(event.getRawY()-initialTouchY);
+                        windowManager.updateViewLayout(overlayIcon,params);    // 위치 갱신
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+        // 시작 아이콘 화면에 띄움
+        windowManager.addView(overlayIcon, params);
     }
 
     @Override
